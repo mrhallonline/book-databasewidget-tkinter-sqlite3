234aeb8eaf3f301902d8e9db8b658ae6d16a53b7
Kevin <mrhallonline@gmail.com>
2019-12-28 22:55:59 -0500

    create table and add to table

--- databaseScript.py
+++ databaseScript.py
@@ -1,7 +1,27 @@
 import sqlite3
 
-conn=sqlite3.connect("books.db")
-cur=conn.cursor()
-cur.execute("CREATE TABLE IF NOT EXISTS books (title TEXT, author TEXT, published INTEGER, isbn INTEGER)")
-conn.commit()
-conn.close()
+def create_table():
+    conn=sqlite3.connect("books.db")
+    cur=conn.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS books (title TEXT, author TEXT, published INTEGER, isbn TEXT)")
+    conn.commit()
+    conn.close()
+    
+def view_table():
+    conn=sqlite3.connect("books.db")
+    cur=conn.cursor()
+    cur.execute("SELECT * FROM books")
+    rows=cur.fetchall()
+    conn.close()
+    return rows
+
+def add_to_table(title, author, published, isbn):
+    conn=sqlite3.connect("books.db")
+    cur=conn.cursor()
+    cur.execute("INSERT INTO books VALUES (?,?,?,?)",(title, author, published, isbn))
+    conn.commit()
+    conn.close()
+
+create_table()
+add_to_table("Concrete Blonde","Michael Connelly",2010, "0316120413")
+print(view_table())
