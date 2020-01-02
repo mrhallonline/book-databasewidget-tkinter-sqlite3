0a98bf0988d88cee24e2d898d7a98b749dd53911
Kevin <mrhallonline@gmail.com>
2020-01-01 21:20:15 -0500

    add text variables, add functions

--- GuiScript.py
+++ GuiScript.py
@@ -12,12 +12,13 @@ def search_entry():
     pass
 
 def add_entry():
+    add_to_table(title_input, author_input, published_input, isbn_input)
+
+def update():
     pass
 
-def update_selected():
-    pass
-
-def delete_selected():
+def delete():
+    
     pass
 
 def close():
@@ -30,17 +31,21 @@ l3 = Label(window, text="Year", width=10).grid(row=1, column=0)
 l4 = Label(window, text="ISBN", width=10).grid(row=1, column=2)
 
 #Entry fields
-e1 = Entry(window, width=25).grid(row=0, column=1)
-e2 = Entry(window, width=25).grid(row=0, column=3)
-e3 = Entry(window, width=25).grid(row=1, column=1)
-e4 = Entry(window, width=25).grid(row=1, column=3)
+title_input=StringVar()
+author_input=StringVar()
+published_input=StringVar()
+isbn_input=StringVar()
+e1 = Entry(window, textvariable=title_input, width=25).grid(row=0, column=1)
+e2 = Entry(window, textvariable=author_input, width=25).grid(row=0, column=3)
+e3 = Entry(window, textvariable=published_input, width=25).grid(row=1, column=1)
+e4 = Entry(window, textvariable=isbn_input, width=25).grid(row=1, column=3)
 
 #buttons
 b1 = Button(window, text="View all", command=view, width=15).place(x=325, y=75)
 b2 = Button(window, text="Search Entry", command=search_entry, width=15).place(x=325, y=100)
 b3 = Button(window, text="Add Entry", command=add_entry, width=15).place(x=325, y=125)
-b4 = Button(window, text="Update Selected", command=update_selected, width=15).place(x=325, y=150)
-b5 = Button(window, text="Delete Selected", command=delete_selected, width=15).place(x=325, y=175)
+b4 = Button(window, text="Update Selected", command=update, width=15).place(x=325, y=150)
+b5 = Button(window, text="Delete Selected", command=delete, width=15).place(x=325, y=175)
 b6 = Button(window, text="Close", command=close, width=15).place(x=325, y=200)
 
 #Scrollbar
@@ -48,15 +53,11 @@ scrollbar = Scrollbar(window)
 scrollbar.place(x=300, y=125 )
 
 #Display field
-
-display = Listbox(window, height=10, width=50)
+display = Listbox(window, height=10, width=45)
 display.place(x=15, y=75)
 
 # Attach scrollbar
 display.config(yscrollcommand=scrollbar.set)
 scrollbar.config(command=display.yview)
-
-
-
 
 window.mainloop()
--- databaseScript.py
+++ databaseScript.py
@@ -21,8 +21,22 @@ def add_to_table(title, author, published, isbn):
     cur.execute("INSERT INTO books VALUES (?,?,?,?)",(title, author, published, isbn))
     conn.commit()
     conn.close()
+    
+def update_selected(title, author, published, isbn):
+    conn=sqlite3.connect("books.db")
+    cur=conn.cursor()
+    cur.execute("UPDATE books SET title=?, author=?, published=?, isbn=?"(title, author, published, isbn))
+    conn.commit()
+    conn.close()
+    
+def delete_selected(item):
+    conn=sqlite3.connect("books.db")
+    cur=conn.cursor()
+    cur.execute("DELETE FROM books WHERE item = ?"(item,))
+    conn.commit()
+    conn.close()
+    
 
 if __name__ == "__main__":
-    create_table()
-    add_to_table("Concrete Blonde","Michael Connelly",2010, "0316120413")
+    # add_to_table("NewTitle","NewAuth", 2020, "0316533213")
     print(view_all())
